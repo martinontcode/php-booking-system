f689a4e66bd218f1ea52c9c76006beb1460d0e72
Martin <martin.onton@gmail.com>
2016-10-27 22:38:54 +0300

    Added header file, updated form .css

--- views/bookingform.php
+++ views/bookingform.php
@@ -1,22 +1,35 @@
-<!-- Include Bootstrap .css ; You can copy this to your header file instead. I've added custom code at the end. -->
-<link rel="stylesheet" href="css/bootstrap.css">
-<!-- Custom -->
-<link rel="stylesheet" href="css/booking-form.css">
+<?php    
+include_once 'header.php'; 
+?>
 
 <div class="container">
     
-<!-- Registration Form -->
-<form method="post" action="index.php" name="bookingform" class="booking-form">
+<!-- Booking Form -->
+<form method="post" action="index.php" name="booking" class="booking-form">
 
 <div class="form-group">
-    <div class="cnt"><h3>User registration</h3></div>
-    <label for="username">Username:</label>
-    <input type="text" name="username" id="username" placeholder="Username" class="input form-control" autocomplete="off" required autofocus><br>
-    <label for="password">Password:</label>
-    <input type="password" name="password" id="password" placeholder="Password" class="input form-control" autocomplete="off" required autofocus><br>
+    <div class="cnt"><h3>Booking</h3></div>
+    <h4>Contact information:</h4>
+    <label for="username">Name:</label>
+    <input type="text" name="username" id="username" placeholder="Full name" class="input form-control" autocomplete="off" required autofocus>
     <label for="email">Email:</label>
-    <input type="email" name="email" id="email" placeholder="Email" class="input form-control" autocomplete="off" required autofocus><br>
-    <input type="submit"  name="registration" value="Sign Up" class="btn btn-lg btn-success btn-block submit" />
+    <input type="email" name="email" id="email" placeholder="Email" class="input form-control" autocomplete="off" required autofocus>
+    <label for="phone">Phone:</label>
+    <input type="text" name="phone" id="phone" placeholder="Phone number" class="input form-control" autocomplete="off" required autofocus><br>
+    <h4>Reservation infomation:</h4>
+    <label for="carlist">Car:</label>
+                <select class="form-control" id="carlist" name="carlist" required autofocus>
+                    <option><!-- Car 1 --> Car 1</option>
+                    <option><!-- Car 2 --> Car 2</option>
+                    <option><!-- Car 3 --> Car 3</option>
+                    <option><!-- Car 4 --> Car 4</option>
+                </select>
+    <label for="startTime">Reservation start:</label>
+    <input type="text" name="startTime" id="startTime" placeholder="14:00" class="input form-control" autocomplete="off" required autofocus>
+    <label for="endTime">Reservation end:</label>
+    <input type="text" name="endTime" id="endTime" placeholder="15:00" class="input form-control" autocomplete="off" required autofocus><br>
+    
+    <input type="submit"  name="booking" value="Book car" class="btn btn-lg btn-book btn-block submit" />
 </div>
     
 </form>
---
+++ views/header.php
@@ -0,0 +1,28 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Car booking system</title>
+    <meta name="viewport" content="width=device-width,  initial-scale=1.0">
+    <!-- Include Bootstrap .css -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Custom -->
+    <link rel="stylesheet" href="css/booking-form.css">
+</head>
+
+<header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-default">
+
+        <div class="container-fluid">
+
+            <div class="container">
+                
+                <!-- Logo -->
+                <a class="navbar-brand" href="index.php" >
+                    <img alt="logo" src="css/logo.png">
+                </a>    <!-- /Logo -->
+                
+            </div>
+        </div>
+    </nav>
+</header>
+
